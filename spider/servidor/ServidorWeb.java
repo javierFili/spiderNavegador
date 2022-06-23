b532374b35e70dc5fe4cb581253b23724811b9c1
javierFili <javierfiligranaagreda@gmail.com>
2022-06-22 21:48:31 -0400

    ahora si da

--- spider/servidor/ServidorWeb.java
+++ spider/servidor/ServidorWeb.java
@@ -11,48 +11,18 @@ public class ServidorWeb {
   private String nombre;
 
   public ServidorWeb(String nombreServidor) {
-    agregarRecursosDisponiblesALista(nombreServidor);
     this.nombre = nombreServidor;
-  }
-
-  private void agregarRecursosDisponiblesALista(String nombreServidor) {
-    FileReader leer;
-    BufferedReader almacenamiento;
-    nombreServidor = nombreServidor.replace('.', '/');
-    String nombreRecurso = "", informacionSacada = "";
-    String pathName = "servidores/" + nombreServidor + "/recursosDisponibles.txt";
-    recursosDisponibles = new File(pathName);
-    long hayContenido = recursosDisponibles.length();
-    if (hayContenido != 0) {
-      try {
-        leer = new FileReader(recursosDisponibles);
-        almacenamiento = new BufferedReader(leer);
-        while (informacionSacada != null) {
-          informacionSacada = almacenamiento.readLine();
-          if (informacionSacada != null) {
-            nombreRecurso = informacionSacada;
-            recursos.add(nombreRecurso);
-          }
-        }
-        almacenamiento.close();
-      } catch (Exception e) {
-        System.out.println(e.toString());
-      }
-    }
   }
 
   public RespuestaHTTP obtenerRespuesta(PedidoHTTP p) {
     String recurso = p.getUrl();
     recurso = obtenerRecurso(recurso);
-    String siNoHubiera = obtenerRecursoHtml("errores", "404.html");
-    RespuestaHTTP respuesta = new RespuestaHTTP(404, siNoHubiera);
-    //if (recursos.contains(recurso)) {
+    RespuestaHTTP respuesta = new RespuestaHTTP(404, para404);
     Pattern pattern = Pattern.compile("^[a-zA-Z.]*");
     Matcher matcher = pattern.matcher(recurso);
     boolean b = matcher.matches();
     if (!b) {
-      siNoHubiera = obtenerRecursoHtml("errores", "400.html");
-      return new RespuestaHTTP(400, siNoHubiera);
+      return new RespuestaHTTP(400, para400);
     }
     if (true) {
       //String nombre = obtenerNombre(this.nombre);
@@ -68,8 +38,9 @@ public class ServidorWeb {
   private String obtenerRecursoHtml(String nombre, String recurso) {
     FileReader leer;
     BufferedReader almacenamiento;
+    System.out.println("buscando el recurso:"+nombre+"y "+recurso);
     String html = "", informacionSacada = "";
-    nombre = nombre.replace('.', '/');
+    //nombre = nombre.replace('.', '/');
     String pathName = "servidores/" + nombre + "/" + recurso;
     recursosDisponibles = new File(pathName);
     long hayContenido = recursosDisponibles.length();
@@ -93,22 +64,53 @@ public class ServidorWeb {
 
   private String obtenerRecurso(String recurso) {
     int i = obtenerPosChar(recurso, ';', 0);
+    if(i==-1){
+       return "2111";
+    }
     recurso = recurso.substring(i + 1, recurso.length());
     return recurso;
   }
 
   private int obtenerPosChar(String cadena, char subCad, int ini) {
     int res = ini;
+    int ress= 0;
     while (res < cadena.length()) {
       if (cadena.charAt(res) == subCad) {
+        ress= res;
         break;
+      }else{
+        ress= -1;
       }
       res++;
     }
-    return res;
+    return ress;
   }
 
   public String getNombre() {
     return nombre;
   }
+
+  private String para400= "<!DOCTYPE html>\n" +
+      "<html lang=\"en\">\n" +
+      "<head>\n" +
+      "    <meta charset=\"UTF-8\">\n" +
+      "    <title>Title</title>\n" +
+      "</head>\n" +
+      "<body>\n" +
+      "   <div>\n" +
+      "       400 - Bad request\n" +
+      "   </div>\n" +
+      "</body>\n" +
+      "</html>";
+  private  String para404= "<!DOCTYPE html>\n" +
+      "<html lang=\"en\">\n" +
+      "<head>\n" +
+      "    <meta charset=\"UTF-8\">\n" +
+      "    <title>Title</title>\n" +
+      "</head>\n" +
+      "<body>\n" +
+      "    404 - Not found\n" +
+      "</body>\n" +
+      "</html>";
+
 }
